b01f06089a477880160e64467f05acb898b74bb4
judeallanhill <judeallanhill@icloud.com>
2023-07-21 13:09:32 -0700

    fix

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.text());
 // CORS middleware to allow requests from http://127.0.0.1:1430
 app.use(
   cors({
-    origin: 'https://apps.hude.earth:1430/data',
+    origin: 'https://apps.hude.earth:1430',
   })
 );
 
